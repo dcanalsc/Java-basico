bb34b5c688207c4d84cd2278a6d03dde26934186
dcanalsc <114098841+dcanalsc@users.noreply.github.com>
2022-09-30 22:16:18 +0200

    Add files via upload

--- UltimoEjercicio/src/com/ejercicio/Punto9.java
+++ UltimoEjercicio/src/com/ejercicio/Punto9.java
@@ -13,21 +13,12 @@ public class Punto9 {
         String masAlumnos;
 
         //AQUI LEE EL FICHERO PARA OBTENER LOS DATOS DE LA CLASE Y LOS PASA A UNA ARRAYLIST
-        try {
-            System.out.print("Leyendo clase del fichero clase.dat.. ");
-            ObjectInputStream leyendoFichero = new ObjectInputStream(
-                    new FileInputStream("clase.dat"));
-            clase = (ArrayList<Alumno>) leyendoFichero.readObject();
-            leyendoFichero.close();
-            System.out.println("ok!");
-            System.out.println("Datos leídos del fichero:");
 
-        } catch (Exception e) {
-            System.out.println("No hay alumno\n" +e.getMessage());
-        }
+        clase = leerFichero(clase);
 
         //-------------BUCLE PARA AÑADIR ALUMNOS A LA CLASE
 
+        boolean guardar = false;
         do {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Añadir alumno?: (s)i o (n)o:");
@@ -37,50 +28,68 @@ public class Punto9 {
                 Alumno alumno = new Alumno();
                 System.out.println("Introduce nombre del alumno:\n");
                 alumno.nombre = scanner.nextLine();
+
+                int j= clase.size();
+                for (int i=0; i<j; i++){
+                    if(alumno.nombre.equals(clase.get(i).nombre)){
+                        System.out.println("El alumno ya existe, se actualizará.");
+                        clase.remove(i);
+                        j--;i--;
+                    }
+                }
                 System.out.println("Introduce las notas:\n");
                 ponerNotas(alumno, scanner);
                 clase.add(alumno);
-
-            } else {
-                System.out.println("No hay mas alumnos");
+                guardar = true;
             }
         } while (masAlumnos.startsWith("s") || masAlumnos.startsWith("S"));
 
-    //AQUI MUESTRA LOS VALORES QUE SE VAN A GUARDAR
+    //AQUÍ MUESTRA LOS VALORES QUE SE VAN A GUARDAR
+
         for (Alumno valor : clase) {
             System.out.println(valor.nombre);
             for (HashMap.Entry<String, Double> elemento : valor.notas.entrySet()) {
                 System.out.println(elemento.getKey() + " : " + elemento.getValue());
             }
         }
-    //AQUI GUARDA CLASE EN FICHERO
-        guardarFichero(clase);
+    //AQUÍ GUARDA CLASE EN FICHERO SI SE HA MODIFICADO
+        if(guardar) guardarFichero(clase);
     }
 
-    // FUNCION QUE PONE LAS NOTAS A LOS ALUMNOS **MEJORABLE
-    private static void ponerNotas(Alumno alumno, Scanner scanner) {
+    //FUNCIÓN PARA LEER ARCHIVO CLASE
+    private static ArrayList<Alumno> leerFichero(ArrayList<Alumno> clase) {
         try {
-            System.out.println("Matematicas:");
-            double nota = scanner.nextDouble();
-            alumno.notas.put("Matematicas", nota);
-            System.out.println("Ciencias:");
-            nota = scanner.nextDouble();
-            alumno.notas.put("Ciencias", nota);
-            System.out.println("Lengua:");
-            nota = scanner.nextDouble();
-            alumno.notas.put("Lengua", nota);
-            System.out.println("Ingles:");
-            nota = scanner.nextDouble();
-            alumno.notas.put("Ingles", nota);
+            System.out.print("Leyendo clase del fichero... ");
+            ObjectInputStream leyendoFichero = new ObjectInputStream(
+                    new FileInputStream("clase.dat"));
+            clase = (ArrayList<Alumno>) leyendoFichero.readObject();
+            leyendoFichero.close();
+            System.out.println("ok!");
+            System.out.println("Datos leídos del fichero:");
+
         } catch (Exception e) {
-            System.out.println(e.getMessage() + " Error, vuelve a introducir alumno!");
+            System.out.println("Clase vacia\n" +e.getMessage());
+        }
+        return clase;
+    }
+
+    // FUNCIÓN QUE PONE LAS NOTAS A LOS ALUMNOS
+    private static void ponerNotas(Alumno alumno, Scanner scanner) {
+        String []asignaturas = {"Matemáticas", "Ciencias", "Lengua", "Ingles", "Filosofía"};
+        try {
+            for (String valor : asignaturas){
+                System.out.println(valor);
+                double nota = scanner.nextDouble();
+                alumno.notas.put(valor, nota);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage() + " Error, vuelve a introducir datos!");
         }
     }
 // FUNCION QUE GUARDA LOS DATOS EN ARCHIVO: clase.dat
     private static void guardarFichero(ArrayList<Alumno> clase) {
         try {
-            System.out.print("Guardando clase en el fichero objetos.dat.. ");
-
+            System.out.print("Guardando clase en el fichero... ");
             ObjectOutputStream escribiendoFichero = new ObjectOutputStream(
                     new FileOutputStream("clase.dat"));
             escribiendoFichero.writeObject(clase);
